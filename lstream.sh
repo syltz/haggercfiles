b3962693793a84acbca054af094c4e90c28b7647
syltz <syltz22@gmail.com>
2015-03-07 12:01:44 +0100

    lstream can now handle hitbox and dailymotion(maybe)

--- lstream.sh
+++ lstream.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-#site=$1
-streamIn=$1
-stream="livestreamer twitch.tv/"$streamIn" high"
+streamIn="$1"
+site="$2"
+if [ "x$site" = "x" ]; then
+    stream="livestreamer twitch.tv/"$streamIn" high"
+elif [ "$site" = "twitch" ]; then
+    stream="livestreamer twitch.tv/"$streamIn" high"
+elif [ "$site" = "hitbox" ]; then
+    stream="livestreamer hitbox.tv/"$streamIn" 720p"
+elif [ "$site" = "dailymotion" ]; then 
+    stream="livestreamer dailymotion.com/"$streamIn" 720p"
+fi
 $stream
 
