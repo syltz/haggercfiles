1966b86cea23373c08e2007852cc355986858238
syltz <syltz22@gmail.com>
2015-03-09 23:00:59 +0100

    Added install python-pip and pip install livestreamer

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir Configs
-sudo apt-get install git vim zsh vlc
+sudo apt-get install git vim zsh vlc python-pip
+sudo pip install livestreamer
 git clone https://github.com/syltz/haggercfiles.git Configs
 ln -s ~/Configs/haggercfiles/.vimrc .vimrc
 ln -s ~/Configs/haggercfiles/.zshrc .zshrc
