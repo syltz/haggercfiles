46f92d5b8b5bfa3f2d1d59fef55f21910283bfa2
syltz <syltz22@gmail.com>
2015-03-09 23:59:40 +0100

    Adding a screenSetup script

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-mkdir Configs
-sudo apt-get install git vim zsh vlc python-pip
+sudo apt-get install vim zsh vlc python-pip
 sudo pip install livestreamer
-git clone https://github.com/syltz/haggercfiles.git Configs
-ln -s ~/Configs/haggercfiles/.vimrc .vimrc
-ln -s ~/Configs/haggercfiles/.zshrc .zshrc
+ln -s ~/Configs/haggercfiles/.vimrc $HOME/.vimrc
+ln -s ~/Configs/haggercfiles/.zshrc $HOME/.zshrc
 
