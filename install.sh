b5616ec92319190b680795e35dcbe2d4a0d680a5
syltz <syltz22@gmail.com>
2015-03-10 00:30:14 +0100

    changes

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
-sudo apt-get install vim zsh vlc python-pip
+# Install programs that I know I want
+sudo apt-get install vim zsh vlc python-pip ktorrent thunderbird steam
 sudo pip install livestreamer
+
+# Create symlinks to the rc-files
 ln -s ~/Configs/haggercfiles/.vimrc $HOME/.vimrc
 ln -s ~/Configs/haggercfiles/.zshrc $HOME/.zshrc
+
+# Set up the screens, will need to change when screens change
 ./screenSetup.sh
+
+# Change shell to ZSH
 chsh -s /bin/zsh
+
+# Download and set up ts3
+wget -P ~/Downloads http://dl.4players.de/ts/releases/3.0.16/TeamSpeak3-Client-linux_amd64-3.0.16.run
+chmod +x ~/Downloads/TeamSpeak3-Client-linux_amd64-3.0.16.run
+./TeamSpeak3-Client-linux_amd64-3.0.16.run
+
+
