798029555ab65c1ffcfc2d6b1c7d4366f548f112
syltz <syltz22@gmail.com>
2015-03-10 00:02:10 +0100

    install.sh now changes shell and sets up the screens

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ sudo apt-get install vim zsh vlc python-pip
 sudo pip install livestreamer
 ln -s ~/Configs/haggercfiles/.vimrc $HOME/.vimrc
 ln -s ~/Configs/haggercfiles/.zshrc $HOME/.zshrc
-
+./screenSetup.sh
+chsh -s /bin/zsh
