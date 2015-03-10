0ec2cccdf56784d2f11fa0f6f2b8d4ba8f885864
syltz <syltz22@gmail.com>
2015-03-10 09:09:51 +0100

    Added symlinks to the lxpanel as well as better commenting

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
-# Install programs that I know I want
+## Install programs that I know I want
 sudo apt-get install vim zsh vlc python-pip ktorrent thunderbird steam
 sudo pip install livestreamer
 
-# Create symlinks to the rc-files
+## Create symlinks to the rc-files
 ln -s ~/Configs/haggercfiles/.vimrc $HOME/.vimrc
 ln -s ~/Configs/haggercfiles/.zshrc $HOME/.zshrc
+# Remove the old panel config file
+rm $HOME/.config/lxpanel/Lubuntu/panels/panel
+ln -s ~/Configs/haggercfiles/panel $HOME/.config/lxpanel/Lubuntu/panels/panel
 
-# Set up the screens, will need to change when screens change
+## Set up the screens, will need to change when screens change
 ./screenSetup.sh
 
-# Change shell to ZSH
+## Change shell to ZSH
 chsh -s /bin/zsh
 
-# Download and set up ts3
+## Download and set up ts3
 wget -P ~/Downloads http://dl.4players.de/ts/releases/3.0.16/TeamSpeak3-Client-linux_amd64-3.0.16.run
+# Make the run-file executable
 chmod +x ~/Downloads/TeamSpeak3-Client-linux_amd64-3.0.16.run
 ./TeamSpeak3-Client-linux_amd64-3.0.16.run
-
 
