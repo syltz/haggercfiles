2d0b670f483063be0c65d9d5a9a00ecd14d02c32
syltz <syltz22@gmail.com>
2015-09-07 21:08:18 +0200

    Twitch now plays in source rather than high.

--- lstream.sh
+++ lstream.sh
@@ -2,9 +2,9 @@
 streamIn="$1"
 site="$2"
 if [ "x$site" = "x" ]; then
-    stream="livestreamer twitch.tv/"$streamIn" high"
+    stream="livestreamer twitch.tv/"$streamIn" source"
 elif [ "$site" = "twitch" ]; then
-    stream="livestreamer twitch.tv/"$streamIn" high"
+    stream="livestreamer twitch.tv/"$streamIn" source"
 elif [ "$site" = "hitbox" ]; then
     stream="livestreamer hitbox.tv/"$streamIn" 720p"
 elif [ "$site" = "dailymotion" ]; then 
