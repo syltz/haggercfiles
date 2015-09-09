46b0c88147f9301f7073fe71407c2e6f2429c72b
syltz <syltz22@gmail.com>
2015-09-09 18:18:47 +0200

    Changed twitch quality back to high from source.

--- lstream.sh
+++ lstream.sh
@@ -2,9 +2,9 @@
 streamIn="$1"
 site="$2"
 if [ "x$site" = "x" ]; then
-    stream="livestreamer twitch.tv/"$streamIn" source"
+    stream="livestreamer twitch.tv/"$streamIn" high"
 elif [ "$site" = "twitch" ]; then
-    stream="livestreamer twitch.tv/"$streamIn" source"
+    stream="livestreamer twitch.tv/"$streamIn" high"
 elif [ "$site" = "hitbox" ]; then
     stream="livestreamer hitbox.tv/"$streamIn" 720p"
 elif [ "$site" = "dailymotion" ]; then 
